04029d6042af99c5b237318d0f3111526001d750
AVogelsanger <voguednj@hotmail.com>
2026-09-18 16:39:06 -0300

    MÓDULO 02 - SPRING - CONTROLE TRANSACIONAL / jpa.hibernate

--- events/src/main/java/br/com/professorisidro/events/controller/ConferenceController.java
+++ events/src/main/java/br/com/professorisidro/events/controller/ConferenceController.java
@@ -1,4 +1,23 @@
 package br.com.professorisidro.events.controller;
 
+import br.com.professorisidro.events.model.Conference;
+import br.com.professorisidro.events.service.IConferenceService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ConferenceController {
+
+    private final IConferenceService service;
+
+    public ConferenceController(IConferenceService service) {
+        this.service = service;
+    }
+
+    @PostMapping("/conferences")
+    public ResponseEntity<Conference> addConference(@RequestBody Conference conference) {
+        return ResponseEntity.status(201).body(service.addConference(conference));
+    }
 }
--- events/src/main/java/br/com/professorisidro/events/controller/UserController.java
+++ events/src/main/java/br/com/professorisidro/events/controller/UserController.java
@@ -1,4 +1,23 @@
 package br.com.professorisidro.events.controller;
 
+import br.com.professorisidro.events.model.User;
+import br.com.professorisidro.events.service.IUserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserController {
+
+    private final IUserService service;
+
+    public UserController(IUserService service) {
+        this.service = service;
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity<User> addUser(@RequestBody User user) {
+        return ResponseEntity.status(201).body(service.addUser(user));
+    }
 }
